623a1a2c0c78a64f1c98305fc28802f71ddd0f59
Dengjl <dengjinlin@sumatrixmed.com>
2024-10-18 15:25:56 +0800

    feat:增加可选外部通信框架

--- ezreg.c
+++ ezreg.c
@@ -2,19 +2,36 @@
 #include "ezreg.h"
 #include "stdlib.h"
 
+
 static ez_reg_t *head = NULL;
 static uint16_t bytes_num_need_save = 0; // count of bytes need to save
 static uint8_t save_flash_check = 0x04;  // check the flash saved flag ,default value is a seed;
+
+#if (USE_EXTERNAL_COMMU == 1)
+#define WAITING_HEAD1     0
+#define WAITING_HEAD2     1
+#define WAITING_ID        2
+#define WAITING_LEN       3
+#define WAITING_RW        4
+#define WAITING_DATA      5
+#define WAITING_CHECK_SUM 6
+
+#define CMD_HEAD1 0x7B
+#define CMD_HEAD2 0xA0
+#define CMD_READ  0x11
+#define CMD_WRITE 0x12
+static uint8_t match_status = 0;
+static uint8_t slave_id = 1;
+#endif
+
 
 ez_reg_t *ez_reg_create(uint16_t start_addr, reg_perm_t perm, void *bindings, uint16_t bytelen);
 ez_reg_t *ez_reg_create_patch(uint16_t start_addr, reg_perm_t perm, void **bindings, uint16_t bind_len, uint16_t bind_size);
 void ez_reg_destroy_all(void);
 void ez_reg_operation(uint16_t addr, uint8_t *data, uint16_t len, reg_op_t op);
-reg_save_load_ret_t ez_reg_save(void);
-reg_save_load_ret_t ez_reg_load(void);
-
-static reg_save_load_ret_t ez_reg_port_save(uint8_t *data, uint16_t len);
-static reg_save_load_ret_t ez_reg_port_load(uint8_t *data, uint16_t len);
+reg_ret_t ez_reg_save(void);
+reg_ret_t ez_reg_load(void);
+reg_ret_t ez_reg_set_slave_id(uint8_t id);
 
 
 /**
@@ -36,7 +53,7 @@ ez_reg_t *ez_reg_create(uint16_t start_addr, reg_perm_t perm, void *bindings, ui
     }
     reg->addr = start_addr;
     reg->bytelen = bytelen;
-    reg->p_reg = (uint8_t*)bindings;
+    reg->p_reg = (uint8_t *)bindings;
     reg->next = NULL;
     reg->perm = perm;
     if ((perm & REG_PERM_S) != 0) {
@@ -145,7 +162,7 @@ void ez_reg_operation(uint16_t addr, uint8_t *data, uint16_t len, reg_op_t op)
  * @brief save all registers with PERM_S
  *
  */
-reg_save_load_ret_t ez_reg_save(void)
+reg_ret_t ez_reg_save(void)
 {
     if (bytes_num_need_save == 0) {
         return REG_SAVE_ERR;
@@ -175,7 +192,7 @@ reg_save_load_ret_t ez_reg_save(void)
  * @brief load all registers with PERM_S
  *
  */
-reg_save_load_ret_t ez_reg_load(void)
+reg_ret_t ez_reg_load(void)
 {
     if (bytes_num_need_save == 0) {
         return REG_LOAD_ERR;
@@ -200,35 +217,99 @@ reg_save_load_ret_t ez_reg_load(void)
         }
         p = p->next;
     }
-    return REG_SAVE_LOAD_OK;
+    return REG_OK;
 }
 
 
-/* ************************* port ******************************* */
-
-/* user include */
-
+#if (USE_EXTERNAL_COMMU == 1)
 /**
- * @brief port function to save
+ * @brief set slave id
  *
- * @param data
- * @param len
- * @return reg_save_load_ret_t
+ * @param id
+ * @return reg_ret_t
  */
-static reg_save_load_ret_t ez_reg_port_save(uint8_t *data, uint16_t len)
+reg_ret_t ez_reg_set_slave_id(uint8_t id)
 {
-    return REG_SAVE_ERR;
+    slave_id = id;
 }
 
 
 /**
- * @brief port function to load
+ * @brief ex_communication
  *
- * @param data
- * @param len
- * @return reg_save_load_ret_t
  */
-static reg_save_load_ret_t ez_reg_port_load(uint8_t *data, uint16_t len)
+void ex_commu_operate_tick(void)
 {
-    return REG_LOAD_ERR;
+    static uint8_t data_buf[CMD_DATA_BUF_LEN];
+    static uint8_t bufp = 0;
+    uint8_t data;
+    while (ez_reg_port_commu_get_a_char(&data) == COMMU_DATA_OK) {
+        switch (match_status) {
+        case WAITING_HEAD1:
+            match_status = (data == CMD_HEAD1 ? match_status + 1 : WAITING_HEAD1);
+            data_buf[0] = CMD_HEAD2;
+            break;
+        case WAITING_HEAD2:
+            match_status = (data == CMD_HEAD2 ? match_status + 1 : WAITING_HEAD1);
+            data_buf[1] = CMD_HEAD1;
+            break;
+        case WAITING_ID:
+            match_status = (data == slave_id ? match_status + 1 : WAITING_HEAD1);
+            data_buf[2] = data;
+            break;
+        case WAITING_LEN:
+            data_buf[3] = data; // data len
+            if (data_buf[3] > CMD_DATA_BUF_LEN - 10) {
+                match_status = WAITING_HEAD1;
+            } else {
+                match_status++;
+            }
+            break;
+        case WAITING_RW:
+            match_status = ((data == CMD_READ || data == CMD_WRITE) ? match_status + 1 : WAITING_HEAD1);
+            data_buf[4] = data;
+            bufp = 5;
+            break;
+        case WAITING_DATA:
+            if (bufp < data_buf[3] + 4) {
+                data_buf[bufp] = data;
+                if (++bufp == data_buf[3] + 4) {
+                    match_status++;
+                }
+            }
+            break;
+        case WAITING_CHECK_SUM: {
+            uint8_t check_sum = 0;
+            for (uint8_t i = 2; i < data_buf[3] + 4; i++) {
+                check_sum = (uint8_t)(check_sum + data_buf[i]);
+            }
+            if (check_sum == data) {
+                uint8_t reg_len = 0;
+                uint8_t check_sum_idx = 0;
+                if (data_buf[4] == CMD_READ) {
+                    reg_len = data_buf[7];
+                    check_sum_idx = 7 + reg_len;
+                    ez_reg_operation(((uint16_t)data_buf[6] << 8) | data_buf[5], &data_buf[7], reg_len, OP_REG_READ);
+                    data_buf[3] = reg_len + 3;
+                } else if (data_buf[4] == CMD_WRITE && data_buf[3] > 3) {
+                    ez_reg_operation(((uint16_t)data_buf[6] << 8) | data_buf[5], &data_buf[7], data_buf[3] - 3, OP_REG_WRITE);
+                    data_buf[3] = 4;
+                    data_buf[7] = 1;
+                    reg_len = 1;
+                    check_sum_idx = 8;
+                }
+                /* check sum and reply */
+                data_buf[check_sum_idx] = 0;
+                for (uint8_t i = 2; i < check_sum_idx; i++) {
+                    data_buf[check_sum_idx] = (uint8_t)(data_buf[check_sum_idx] + data_buf[i]);
+                }
+                ez_reg_port_commu_send(data_buf, reg_len + 8);
+            }
+            match_status = WAITING_HEAD1;
+        } break;
+        default:
+            break;
+        }
+    }
 }
+#endif
---
+++ ezreg_port.h
@@ -0,0 +1,26 @@
+#ifndef _EZREG_PORT_H_
+#define _EZREG_PORT_H_
+
+#include "stdint.h"
+
+/* config */
+#define USE_EXTERNAL_COMMU 1
+#define CMD_DATA_BUF_LEN           70
+
+
+typedef enum {
+    REG_OK = 0,
+    REG_SAVE_ERR,
+    REG_LOAD_ERR,
+    COMMU_NO_DATA,
+    COMMU_DATA_OK,
+} reg_ret_t;
+
+extern reg_ret_t ez_reg_port_save(uint8_t *data, uint16_t len);
+extern reg_ret_t ez_reg_port_load(uint8_t *data, uint16_t len);
+#if (USE_EXTERNAL_COMMU == 1)
+extern void ez_reg_port_commu_send(uint8_t *data, uint16_t len);
+extern uint8_t ez_reg_port_commu_get_a_char(uint8_t *data);
+#endif
+
+#endif
